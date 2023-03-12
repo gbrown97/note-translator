061eaf067067fefc3c8e4a81fa1a9a90ed9d3473
uday <patlolla.25@wright.edu>
2023-03-11 21:04:26 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="../public/noteTrans.jpg">
+  <noteTransl msg="Welcome to Note Translation"/>
+</template>
+
+<script>
+import noteTransl from './components/noteTransl.vue'
+
+export default {
+  name: 'App',
+  components: {
+    noteTransl
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#Helloworld {
+  text-align: right;
+}
+</style>
---
+++ src/components/noteTransl.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <br>
+    <form>
+    <input type="text" placeholder="Username">
+    <br>
+    <br>
+    <input type="password" placeholder="password">
+    <br>
+    <br>
+      <input  type="button" value="Login" >
+      <input  type="button" value="SignUp">
+    </form>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+input {
+color: #2c3e50;
+  font-size: x-large;
+  padding: 15px;
+  border-radius: 25px;
+  width: 300px;
+  height: 20px
+}
+input[type="button"]{
+
+  color: #2c3e50;
+  margin-right: 25px;
+  font-size: x-large;
+  font-weight: bold;
+  background-color: aquamarine;
+  border-radius: 25px;
+  width: 120px;
+  height: 50px
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
